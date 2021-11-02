3c0479891701744a6c7836be0525fc8a648ef347
AlexZhu <alexzhu2001@stu.xupt.edu.cn>
2021-11-02 20:49:12 +0800

    Add something

--- code/Gen_Square_Wave.m
+++ code/Gen_Square_Wave.m
@@ -34,13 +34,17 @@ xticks(-3:1:3);
 yticks(-1:1:1);
 grid on;
 hp=plot(x,0);
-for i=1:1:10
-    delete(hp);
+m=0;
+for i=1:1:30
     fif_sin=fif_sin+4/pi*1/(2.*i-1).*sin(omg.*(2.*i-1).*x);
-    ss=sprintf("No.%d Composition",i);
-    title(ss)
-    hp=plot(x,fif_sin);
-    pause(0.5);
-    str=sprintf("../figure/fig_2/fig-%d.png",i);
-    saveas(hp,str);
+    if mod(i,5)==0
+        delete(hp);
+        ss=sprintf("No.%d Composition",i);
+        title(ss)
+        hp=plot(x,fif_sin);
+        pause(0.5);
+        str=sprintf("../figure/fig_5/fig-%d.png",m);
+        saveas(hp,str);
+        m=m+1;
+    end
 end
